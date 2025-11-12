f7600f780bccbef64dc26510e3bfe35200f8da6f
Mehmet <mehmet.sarioglu.tr@gmail.com>
2025-11-13 01:52:55 +0300

    update efficacy services

--- src/api/efficacy-assessment/services/efficacy-assessment.ts
+++ src/api/efficacy-assessment/services/efficacy-assessment.ts
@@ -3,5 +3,130 @@
  */
 
 import { factories } from '@strapi/strapi';
+const assessment_finished_check = (answers) => {
+  try {
+    const result = answers.every((q) => q.answer !== 0);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+const assessment_calulate_score = (answers) => {
+  const grouped_answerd = answers.reduce((acc, item) => {
+    const { section_group, answer } = item;
+    if (!acc[section_group]) {
+      acc[section_group] = 0;
+    }
+    acc[section_group] += answer;
+    return acc;
+  }, {});
 
-export default factories.createCoreService('api::efficacy-assessment.efficacy-assessment');
+  return grouped_answerd;
+};
+export default factories.createCoreService(
+      "api::efficacy-assessment.efficacy-assessment",
+  ({ strapi }) => ({
+    getUserEfficiencyAssessment: async (userId) => {
+      try {
+        const userEfficiencyAssessment = await strapi.entityService.findMany(
+          "api::efficacy-assessment.efficacy-assessment",
+          {
+            filters: { user: userId },
+          }
+        );
+
+        if (!userEfficiencyAssessment.length) {
+          return {
+            success: false,
+            message: "No efficiency assessment found",
+          };
+        }
+
+        let assessment = userEfficiencyAssessment[0];
+
+        // Check if `results` exists and is not null
+        if (assessment.results) {
+          // Define the required order
+          const order = ["VDS", "PE", "CE", "CEf", "LDS"];
+
+          // Sort the results based on the predefined order
+          assessment.results = Object.fromEntries(
+            order
+              .filter((key) => assessment.results[key] !== undefined) // Keep only existing keys
+              .map((key) => [key, assessment.results[key]]) // Convert to [key, value] pairs
+          );
+        }
+
+        return {
+          success: true,
+          data: assessment, // Return the updated assessment with sorted results
+        };
+      } catch (error) {
+        throw error;
+      }
+    },
+    saveUserEfficacyAssessment: async (ctx) => {
+      try {
+        const userEfficiencyAssessment = await strapi.entityService.findMany(
+          "api::efficacy-assessment.efficacy-assessment",
+          {
+            filters: {
+              user: ctx.state.user.id,
+            },
+          }
+        );
+        if (userEfficiencyAssessment.length) {
+          const res = await strapi.entityService.update(
+            "api::efficacy-assessment.efficacy-assessment",
+            userEfficiencyAssessment[0].id,
+            {
+              data: {
+                answers: ctx.request.body,
+              },
+            }
+          );
+          if (assessment_finished_check(res.answers)) {
+            const scores = assessment_calulate_score(res.answers);
+            console.log("scores", scores);
+            const updateAssessment = await strapi.entityService.update(
+              "api::efficacy-assessment.efficacy-assessment",
+              res.id,
+              {
+                data: {
+                  results: scores,
+                },
+              }
+            )
+            // console.log("scores", updateAssessment);
+          }
+          return res;
+        }
+        const res = await strapi.entityService.create(
+          "api::efficacy-assessment.efficacy-assessment",
+          {
+            data: {
+              answers: ctx.request.body,
+              user: ctx.state.user.id,
+            },
+          }
+        );
+        if (assessment_finished_check(res.answers)) {
+          const scores = assessment_calulate_score(res.answers);
+          const updateAssessment = await strapi.entityService.update(
+            "api::efficacy-assessment.efficacy-assessment",
+            res.id,
+            {
+              data: {
+                results: scores,
+              },
+            }
+          )
+          // console.log("scores", updateAssessment);
+        }
+        return res;
+      } catch (error) {
+        throw error;
+      }
+    },
+  })
+);
--- src/api/efficacy-evaluation-question/services/efficacy-evaluation-question.ts
+++ src/api/efficacy-evaluation-question/services/efficacy-evaluation-question.ts
@@ -4,4 +4,17 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::efficacy-evaluation-question.efficacy-evaluation-question');
+export default factories.createCoreService(
+    "api::efficacy-evaluation-question.efficacy-evaluation-question",
+  ({ strapi }) => ({
+    getEfficacyAssessmentQuestions: async () => {
+      const efficacyAssessmentQuestions = await strapi.entityService.findMany(
+        "api::efficacy-evaluation-question.efficacy-evaluation-question",
+        {
+          fields: ["questions"],
+        }
+      );
+      return efficacyAssessmentQuestions.questions;
+    },
+  })
+);
